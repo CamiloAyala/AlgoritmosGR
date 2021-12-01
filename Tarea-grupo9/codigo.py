304c40637672fffe3108f9bec3a3b439c5b45601
FredyRosero <faroseroc@unal.edu.co>
2021-12-01 14:11:47 -0500

    Print de depuarción

--- Tarea-grupo9/codigo.py
+++ Tarea-grupo9/codigo.py
@@ -7,6 +7,10 @@ class newNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def __str__(self):
+        return str(self.data)
+        #return "{}:(i:{},d:{})".format(self.data,self.left,self.right)
  
 # Function to check the given
 # key exist or not
@@ -29,18 +33,22 @@ def iterativeSearch(root, key):
  
 # A utility function to insert a
 # new Node with given key in BST
-def insert(Node, data):
-     
+def insert(Node, data, parent=None, dir="", tab=""):
+    print(tab+"insert "+str(data),"in "+dir,parent) #depuración
     # If the tree is empty, return
     # a new Node
     if Node == None:
-        return newNode(data)
+        Node = newNode(data)
+        print(data, "inserted!\n") #depuración
+        return Node
  
     # Otherwise, recur down the tree
+    NodeOld = Node
+    tab="\t"+tab #depuración
     if data < Node.data:
-        Node.left = insert(Node.left, data)
+        Node.left = insert(Node.left,data, Node,"izq.",tab)
     elif data > Node.data:
-        Node.right = insert(Node.right, data)
+        Node.right = insert(Node.right,data, Node,"der.",tab)
  
     # return the (unchanged) Node pointer
     return Node
@@ -49,10 +57,13 @@ def insert(Node, data):
 if __name__ == '__main__':
      
     # Let us create following BST
-    # 50
-    # 30     70
-    # / \ / \
-    # 20 40 60 80
+    #          50
+    #       /      \
+    #      30      70
+    #     /  \    /  \
+    #    20  40  60  80
+    #   /
+    # 10
     root = None
     root = insert(root, 50)
     insert(root, 30)
@@ -61,6 +72,8 @@ if __name__ == '__main__':
     insert(root, 70)
     insert(root, 60)
     insert(root, 80)
+    insert(root, 10)
+    
     if iterativeSearch(root, 15):
         print("Yes")
     else:
