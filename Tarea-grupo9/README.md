e461ff421119eaa4880092943e96c531fb34dec2
FredyRosero <faroseroc@unal.edu.co>
2021-12-05 22:13:08 -0500

    Final

--- Tarea-grupo9/README.md
+++ Tarea-grupo9/README.md
@@ -1,8 +1,8 @@
 Edite el código para crear el árbol desde entradas de usuario:
-* Definir Raíz
-* Definir Cantidad de Nodos
-* Establecer los valores de los nodos.
-* Indicar búsqueda.
+- [X] Definir Raíz
+- [X] Definir Cantidad de Nodos
+- [X] Establecer los valores de los nodos.
+- [X] Indicar búsqueda.
 
 El algoritmo debe mostrar la ruta encontrada en el grafo ejemplo:
 Puede proponer una manera más clara de representar el grafo y ruta en el grafo(opcional 10 puntos de participación).
--- Tarea-grupo9/codigo.py
+++ Tarea-grupo9/codigo.py
@@ -7,13 +7,12 @@ class newNode:
         self.data = data
         self.left = None
         self.right = None
-
-    def __str__(self):
-        return str(self.data)
-        #return "{}:(i:{},d:{})".format(self.data,self.left,self.right)
  
 # Function to check the given
 # key exist or not
+
+ruta = list("")
+
 def iterativeSearch(root, key):
      
     # Traverse until root reaches
@@ -22,10 +21,12 @@ def iterativeSearch(root, key):
          
         # pass right subtree as new tree
         if key > root.data:
+            ruta.append("derecha")
             root = root.right
  
         # pass left subtree as new tree
         elif key < root.data:
+            ruta.append("izquierda")
             root = root.left
         else:
             return True # if the key is found return 1
@@ -33,22 +34,18 @@ def iterativeSearch(root, key):
  
 # A utility function to insert a
 # new Node with given key in BST
-def insert(Node, data, parent=None, dir="", tab=""):
-    print(tab+"insert "+str(data),"in "+dir,parent) #depuración
+def insert(Node, data):
+     
     # If the tree is empty, return
     # a new Node
     if Node == None:
-        Node = newNode(data)
-        print(data, "inserted!\n") #depuración
-        return Node
+        return newNode(data)
  
     # Otherwise, recur down the tree
-    NodeOld = Node
-    tab="\t"+tab #depuración
     if data < Node.data:
-        Node.left = insert(Node.left,data, Node,"izq.",tab)
+        Node.left = insert(Node.left, data)
     elif data > Node.data:
-        Node.right = insert(Node.right,data, Node,"der.",tab)
+        Node.right = insert(Node.right, data)
  
     # return the (unchanged) Node pointer
     return Node
@@ -57,26 +54,42 @@ def insert(Node, data, parent=None, dir="", tab=""):
 if __name__ == '__main__':
      
     # Let us create following BST
-    #          50
-    #       /      \
-    #      30      70
-    #     /  \    /  \
-    #    20  40  60  80
-    #   /
-    # 10
+    #    50
+    # 30     70
+    # / \ / \
+    # 20 40 60 80
     root = None
-    root = insert(root, 50)
+
+    """
+    
     insert(root, 30)
     insert(root, 20)
     insert(root, 40)
     insert(root, 70)
     insert(root, 60)
     insert(root, 80)
-    insert(root, 10)
-    
-    if iterativeSearch(root, 15):
-        print("Yes")
+    """
+     
+    raiz=int(input("Ingrese el valor raiz para la generacion del arbol binario: "))
+    root = insert(root, raiz)
+
+    cant=int(input("Ingrese la cantidad de nodos para la generacion del arbol binario: "))
+    for i in range(cant):
+      
+      val=int(input("Ingrese el valor("+str(i+1)+"): "))
+      insert(root, val)
+
+    num=int(input("Ingrese el valor a buscar en el arbol Binario: "))
+
+
+    print("")
+    if iterativeSearch(root, num):
+        print("El valor se encuentra dentro del arbol!")
+        print("La ruta es: ")
+        for l in ruta:
+          print(l)
+
     else:
-        print("No")
+        print("El valor no se encuentra en el arbol")
  
-# This code is contributed by PranchalK
+  
